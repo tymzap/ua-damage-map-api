f2d380b991d9c39e2387e6d7ae32d978b4c9d318
Tymek Zapała <tymzap@gmail.com>
2022-03-19 15:00:23 +0100

    Adjust CORS

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,11 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: '*',
+    },
+  });
   const configService: ConfigService<Environment> = app.get(ConfigService);
   await app.listen(configService.get('PORT'));
 }
