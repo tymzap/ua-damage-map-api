3898aa3de99704c0a34024f46d5b352598c03216
Tymek Zapała <tymzap@gmail.com>
2022-03-19 18:40:46 +0100

    Fix CreateDamageReport migration name

--- src/migrations/1647706480401-CreateDamageReport.ts
+++ src/migrations/1647706480401-CreateDamageReport.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class CreateDamageDegree1647706480401 implements MigrationInterface {
-  name = 'CreateDamageDegree1647706480401';
+export class CreateDamageReport1647706480401 implements MigrationInterface {
+  name = 'CreateDamageReport1647706480401';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
